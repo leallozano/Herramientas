c66b1c4b683725dcc3490ac212e81852f66be47f
Carlos <leal.lozano@gmail.com>
2017-06-01 20:43:08 -0400

    Tools Camera

--- app/src/main/java/com/example/caleal/herramientas/Nivel.java
+++ app/src/main/java/com/example/caleal/herramientas/Nivel.java
@@ -1,6 +1,12 @@
 package com.example.caleal.herramientas;
 
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,19 +17,74 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Nivel extends Fragment {
+public class Nivel extends Fragment implements SensorEventListener {
 
+    private SensorManager miManager;
+
+    private Sensor miSensor;
+
+    private NivelPantalla pantalla;
 
     public Nivel() {
         // Required empty public constructor
     }
 
 
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        miManager=(SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        miSensor=miManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        int lado=getResources().getDimensionPixelSize(R.dimen.maximo);
+
+        pantalla = new NivelPantalla (getActivity(),lado);
+
+    }
+
+
+    public void onResume(){
+
+        super.onResume();
+
+        miManager.registerListener(this,miSensor,SensorManager.SENSOR_DELAY_GAME);
+
+    }
+
+
+    public void onPause(){
+
+        super.onPause();
+
+        miManager.unregisterListener(this);
+
+    }
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nivel, container, false);
+        //return inflater.inflate(R.layout.fragment_nivel, container, false);
+        return pantalla;
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+
+        pantalla.angulos(event.values);
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 
 }
